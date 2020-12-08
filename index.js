20949e6c92e67acd879b4af284a5d0a15e21fb97
alisonchou <alchousr21@herricksk12.org>
2020-12-08 12:09:31 -0500

    backend server

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+let persons = [
+    {
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1,
+    },
+    {
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: 2,
+    },
+    {
+        name: "Dan Abramov",
+        number: "12-43-234345",
+        id: 3,
+    },
+    {
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+        id: 4,
+    },
+]
+
+app.get('/info', (request, response) => {
+    const currentDate = new Date()
+    response.send(`<div>
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>
+    </div>`)
+})
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body // json-parser turns JSON of request to JS object and stores in .body
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name and/or number missing'
+        })
+    }
+    if (persons.some(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: Math.ceil(Math.random()*999),
+    }
+    persons = persons.concat(person)
+    response.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})

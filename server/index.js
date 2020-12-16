3f5c232a172da47697dcd0ec072b92e1aa47de28
alisonchou <alchousr21@herricksk12.org>
2020-12-15 23:46:39 -0500

    fetch and save to database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config() //env vars available globally
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
--- server/models/person.js
+++ server/models/person.js
@@ -1,4 +1,3 @@
-require('dotenv').config()
 const mongoose = require('mongoose')
 
 const url = process.env.MONGODB_URI
